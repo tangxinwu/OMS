d8b29372ffc2b334a33416b3e537344994ce8c3a
tangxinwu <tangxinwu@outlook.com>
2019-08-29 15:59:11 +0800

    增加自助申请界面的美化

--- infrastructure/statics/js/self_invoke.js
+++ infrastructure/statics/js/self_invoke.js
@@ -6,11 +6,23 @@ $(function () {
     $("#start_invoke").off("onclick").on("click", function () {
        var application_id = $("#selected_appplication").val();
        var auditing_id = $("#auditing_id").val();
-       if (confirm("确定要发送更新申请吗?")){
-           $.post("/self_invoke/", {"application_id": application_id, "action": "web_check", "auditing_id": auditing_id}, function (result) {
-              alert(result);
-           });
-       }
+		 swal({
+			title: "您确定要更新吗？",
+			text: "您确定要更新这个项目？",
+			type: "warning",
+			showCancelButton: true,
+			closeOnConfirm: false,
+			confirmButtonText: "是的，我要更新",
+			confirmButtonColor: "#ec6c62"
+			}, function() {
+                   $.post("/self_invoke/", {"application_id": application_id, "action": "web_check", "auditing_id": auditing_id}, function (result) {
+                        if (result == "非法传值"){
+                        	swal("OMG", result + " 请检查更新的项目是否选中!", "error");
+						} else {
+                        	swal("操作成功!", result, "success");
+						};
 
+                   });
+			});
     });
 });
